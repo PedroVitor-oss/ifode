1b935b3a67228d091c89eb110e75f5e2c9894219
PedroVitor-oss <pedro.santos22@escola.pr.edu.br>
2022-12-02 22:32:39 -0300

    sing_in and log_in

--- index.js
+++ index.js
@@ -17,7 +17,9 @@ const fs = require('fs');
 const bodyPar = require('body-parser');
 app.use(express.json({limit: '20mb'}));
 app.use(express.urlencoded({ extended: false, limit: '20mb' }));
-
+//express session
+const session = require('express-session');
+app.use(session({secret: 'ifodeApp'}));
 fs.readFile('./data/contas.json','utf-8',(erro,data)=>{
         contas = JSON.parse(data);
        // console.log(data)
@@ -25,7 +27,43 @@ fs.readFile('./data/contas.json','utf-8',(erro,data)=>{
 
 
 app.get('/',(req,res)=>{
-        res.render("home",{contas:contas});
+        res.render("home",{conta:req.session.login});
+        console.log(req.session.login)
+})
+app.get("/singin",(req,res)=>{
+        res.render("singin",{contas:contas});
+})
+app.get("/login",(req,res)=>{
+        res.render("login",{contas:contas});
+})
+app.get("/json",(req,res)=>{
+        res.send(contas);
+})
+app.post("/login",(req,res)=>{
+        for(conta of contas){
+                if(conta.email == req.body.email){
+                        if(conta.senha == req.body.password){
+                           req.session.login = conta;
+                           break;  
+                        }
+                }
+        }
+        res.render("certo");
+})
+app.post("/createCont",(req,res)=>{
+        let createConta = {
+                nome:req.body.name,
+                email:req.body.email,
+                senha:req.body.password,
+               premium:req.body.premium,
+        }
+        req.session.login = createConta;
+        contas.push(createConta);
+        fs.writeFile('./data/contas.json',JSON.stringify(contas,null,2),()=>{
+          console.log(createConta)
+        })
+           
+        res.render("certo");
 })
 
 
